5bd7d32073b77cca2321ee37aeb4225d7deb6524
hoseintahan84 <hoseintahan84@gmail.com>
2023-01-05 15:22:19 +0330

    Change file Name And Add SharePerfrances Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,22 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:sport_application/BodyLevel_Page.dart';
-import 'package:sport_application/Day_Page.dart';
-import 'package:sport_application/Exercise_Page.dart';
-import 'package:sport_application/Loading_Page.dart';
-import 'package:sport_application/Login_Page.dart';
-import 'package:sport_application/Ready_Page.dart';
-import 'package:sport_application/Sign_Up.dart';
+import 'package:sport_application/VideoPlayer_Page.dart';
 import 'package:sport_application/gender_Page.dart';
 import 'package:sport_application/model/data/appdata.dart';
-import 'package:sport_application/scrol.dart';
 import 'package:sport_application/setting/Menu_Page.dart';
 
-void main() {
+void main() async {
+  bool? isview = false;
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  isview = await sharedPreferences.getBool("isviewonboarding");
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FutureBuilder<bool>(
-        future: access_data(),
-        builder: (context, snapshot) {
-            if (snapshot.data == true) {
-              return signUp_Page();
-            } else {
-              return home();
-            }
-        },
-      ),
+      home: isview != true ? home() : VideoPlayer_Page(),
     ),
   );
 }
@@ -44,6 +33,7 @@ class _homeState extends State<home> {
   final image_Path = appDataBase().imagePath;
   final content_Page = appDataBase().contentPageView;
   int page = 0;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -58,6 +48,11 @@ class _homeState extends State<home> {
   }
 
   @override
+  OnboardingInfo() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool("isviewonboarding", true);
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -123,20 +118,21 @@ class _homeState extends State<home> {
                         minimumSize: Size(56, 56),
                         primary: Color(0xff4FAF30),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          if (page == image_Path.length - 1) {
+                      onPressed: () async {
+                        if (page == image_Path.length - 1) {
+                          await OnboardingInfo();
+                          setState(() {
                             Navigator.of(context).push(
                               MaterialPageRoute(builder: (contex) {
                                 return MyApp();
                               }),
                             );
-                          } else {
-                            pageController.animateToPage(page + 1,
-                                duration: Duration(milliseconds: 500),
-                                curve: Curves.decelerate);
-                          }
-                        });
+                          });
+                        } else {
+                          pageController.animateToPage(page + 1,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.decelerate);
+                        }
                       },
                       child: Icon(
                         page == image_Path.length - 1
@@ -174,15 +170,5 @@ class _MyAppState extends State<MyApp> {
       backgroundColor: Colors.white,
       body: gender_Page(),
     );
-  }
-}
-
-Future<bool> access_data() async {
-  final prefs = await SharedPreferences.getInstance();
-  final bool? is_view_splash = prefs.getBool('is_splash');
-  if (is_view_splash == true) {
-    return true;
-  } else {
-    return false;
   }
 }
