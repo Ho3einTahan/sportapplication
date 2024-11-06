5194ad690ab88e3bc724c8df18f5a035d8a55981
ho3eintahan <hoseintahan84@gmail.com>
2024-11-06 13:58:36 +0330

    Update    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:sport_application/Acounting/Verification_code_Page.dart';
-import 'package:sport_application/BottomNav/Chat_Page.dart';
-import 'package:sport_application/BottomNav/Home_page.dart';
-import 'package:sport_application/Conection_Internet/check_Conectivity.dart';
+import 'package:sizer/sizer.dart';
 import 'package:sport_application/Qustions/gender_Page.dart';
 import 'package:sport_application/setting/MaterialData.dart';
-import 'package:sizer/sizer.dart';
-import 'Acounting/Forgot_Password_Page.dart';
-import 'Acounting/login_Page.dart';
-import 'BottomNav/PvChat_Page.dart';
 
 void main() async {
   await GetStorage.init();
@@ -21,20 +13,15 @@ void main() async {
       return GetMaterialApp(
         debugShowCheckedModeBanner: false,
         theme: CTheme(),
-        home: PVchat_Page(),
+        home: MyApp(),
       );
     }),
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
